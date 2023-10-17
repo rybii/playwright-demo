be36f7c80fa25d91d092900dcf6244f3d2f9ffac
Volodymyr Rybii <rybii.rv@pg.com>
2023-10-17 18:47:15 +0300

    Added 2 e2e tests

--- tests/e2e/e2e-login.spec.ts
+++ tests/e2e/e2e-login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test.describe("Login/Logout Flows", () =>{
+test.describe.parallel("Login/Logout Flows", () =>{
     test.beforeEach(async ({page}) =>{
         await page.goto('http://zero.webappsecurity.com/')
     })
@@ -12,5 +12,18 @@ test.describe("Login/Logout Flows", () =>{
         await page.click("text=Sign in")
         const errorMessage = await page.locator(".alert-error")
         await expect(errorMessage).toContainText("Login and/or password are wrong.")
+    })
+
+    test("Positive scenario for login + logout", async ({page}) =>{
+        await page.click("#signin_button")
+        await page.type('#user_login', 'username')
+        await page.type('#user_password', 'password')
+        await page.click("text=Sign in")
+        await page.goto('http://zero.webappsecurity.com/bank/transfer-funds.html')
+        const accountSummaryTab = await page.locator('#account_summary_tab')
+        await expect(accountSummaryTab).toBeVisible()
+
+        await page.goto("http://zero.webappsecurity.com/logout.html")
+        await expect(page).toHaveURL("http://zero.webappsecurity.com/index.html")
     })
 })
