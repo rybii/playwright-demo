029a237f9648d2c6eb045e0910260c457cb10c1f
Volodymyr Rybii <rybii.rv@pg.com>
2023-10-18 13:36:39 +0300

    added 1 new specs with e2e tests: Filter Transactions

--- tests/e2e/e2e-transfer-funds.spec.ts
+++ tests/e2e/e2e-transfer-funds.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test.describe.only('Transfer funds and Make Payments', () => {
+test.describe('Transfer funds and Make Payments', () => {
     test.beforeEach(async ({page}) => {
         await page.goto('http://zero.webappsecurity.com/index.html')
         await page.click("#signin_button")
