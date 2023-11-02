e912173734e52b238662fb7984678b98b6a62974
Volodymyr Rybii <rybii.rv@pg.com>
2023-11-02 17:15:22 +0200

    tips and tricks tests introduced & addded

--- tests/tips/tips.spec.ts
+++ tests/tips/tips.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test'
 
-test.describe.only("Tips and Tricks", () => {
+test.describe("Tips and Tricks", () => {
     test("TestInfo Object", async ({page}, testInfo) => {
         await page.goto("https://www.example.com")
         console.log(testInfo.title)
@@ -10,4 +10,40 @@ test.describe.only("Tips and Tricks", () => {
         test.skip(browserName === "chromium", "Feature not ready in Chrome browser")
         await page.goto("https://www.example.com")
     })
+
+    test("Test Fixme Annotation", async ({page, browserName}) => {
+        test.fixme(browserName === "chromium", "Unstable test. Needs revision")
+        await page.goto("https://www.example.com")
+    })
+
+    const people = ["Mike", "John", "Jack", "Andrew", "Bob"]
+    for (const name of people)
+        test(`Running test for ${name}`, async ({page}) => {
+            await page.goto("http://zero.webappsecurity.com/index.html")
+            await page.type("#searchTerm", `${name}`)
+            await page.waitForTimeout(3000)
+        })
+
+    test("Mouse movement simulation", async ({page}) => {
+        await page.goto("https://www.example.com")
+        await page.mouse.move(0,0)
+        await page.mouse.down()
+        await page.mouse.move(0,100)
+        await page.mouse.up()
+    })
+
+    test.only("Multiple browser tabs", async ({browser}) => {
+        const context = await browser.newContext()
+        const page1 = await context.newPage()
+        const page2 = await context.newPage()
+        const page3 = await context.newPage()
+        await page1.goto("https://www.example.com")
+        await page2.goto("https://www.example.com")
+        await page3.goto("https://www.example.com")
+        await page1.waitForTimeout(5000)
+    })
+
+    //Emulate iphone 11: npx playwright open --device="iPhone 11" wikipedia.org
+
+    //Save website to PDF: npx playwright pdf {website URL} {name of the otput file .pdf}
 })
